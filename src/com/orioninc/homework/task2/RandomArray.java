4866e1bad7f063e195c03b0292cdb0cfcb3785f7
Azamat Abdullin <azamat.abdullin@ringcentral.com>
2022-07-31 20:32:12 +0200

    homework 2, task 1

---
+++ src/com/orioninc/homework/task2/RandomArray.java
@@ -0,0 +1,49 @@
+package com.orioninc.homework.task2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomArray {
+    int[] array;
+    int sum;
+    int min;
+    int minIndex;
+
+    RandomArray(int size) {
+        array = new int[size];
+        Random random = new Random();
+        int minElement = Integer.MAX_VALUE;
+        int minElementIndex = 0;
+
+        for (int i = 0; i < size; i += 1) {
+            int element = random.nextInt();
+            array[i] = element;
+            sum += element;
+
+            if (element < minElement) {
+                minElement = element;
+                minElementIndex = i;
+            }
+        }
+
+        min = minElement;
+        minIndex = minElementIndex;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(array);
+    }
+
+    int getSum() {
+        return sum;
+    }
+
+    int getMinElement() {
+        return min;
+    }
+
+    int getMinElementIndex() {
+        return minIndex;
+    }
+}
---
+++ src/com/orioninc/homework/task2/Task1.java
@@ -0,0 +1,21 @@
+package com.orioninc.homework.task2;
+
+//Задание 1. Случайности складываются
+//        - Создать массив на 10 чисел типа int
+//        - Заполнить массив случайными числами, используя класс Random и функцию nextInt()
+//        - Вывести массив на экран одной строкой, разделив элементы пробелом
+//        - Посчитать сумму всех элементов и вывести на экран
+//        - Найти минимальный элемент и вывести на экран его индекс в массиве и значение
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        final int arraySize = 10;
+
+        RandomArray array = new RandomArray(arraySize);
+
+        System.out.println("array: " + array);
+        System.out.println("sum of array elements: " + array.getSum());
+        System.out.println("min element index: " + array.getMinElementIndex() + ", min element value: " + array.getMinElement());
+    }
+}
