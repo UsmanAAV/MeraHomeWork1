16404a44e07f9f00ac21907c65cc82486c7e7055
Azamat Abdullin <azamat.abdullin@ringcentral.com>
2022-08-09 00:00:52 +0200

    Homework 4, task 1 - improved Color enum

--- src/com/orioninc/homework/homework4/Color.java
+++ src/com/orioninc/homework/homework4/Color.java
@@ -4,4 +4,16 @@ package com.orioninc.homework.homework4;
  * Enum for colors of the fruits. 'RED', 'GREEN' or 'YELLOW'.
  */
 
-public enum Color { RED, GREEN, YELLOW }
+public enum Color {
+  RED, GREEN, YELLOW;
+
+  @Override
+  public String toString() {
+    switch (this) {
+      case RED: return "Red";
+      case GREEN: return "Green";
+      case YELLOW: return "Yellow";
+      default: throw new RuntimeException("Unprocessed color in the Color enum");
+    }
+  }
+}
